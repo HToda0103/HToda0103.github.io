74b1f3673c29976cf94189b601c050a9e03ac0d9
HToda0103 <dm256216@cc.seikei.ac.jp>
2025-06-18 11:53:52 +0000

    fix:footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ function Header(){
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#services">Services</a></li>
-        <li><a href="#research thema">Research_thema</a></li>
+        <li><a href="#research thema">Research thema</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
